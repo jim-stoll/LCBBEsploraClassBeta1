bca808821dfae234c6dd843ad09cc8e7a6a6262d
Jim Stoll <jim_stoll@yahoo.com>
2017-05-31 21:10:30 -0400

    - various cleanups - minor refactoring

--- BreakOut/BreakOut.ino
+++ BreakOut/BreakOut.ino
@@ -10,13 +10,7 @@
 //enum resultEnum {LOSS, WIN};
 
 //TODO:
-//BUG: ball sometimes get stuck along right-hand edge in auto - just bounces up/down repeatedly
-//ENHANCEMENT: consider color-coded brick rows, with different point values by row(s)
-//ENHANCEMENT: consider progressive mode
-//ENHANCEMENT: consider allowing blank space above top row
-//ENHANCEMENT: consider further dividing paddle, to allow more gradual diagonal trajectory closer to center
 //ENHANCEMENT: consider bonus drops
-//ENHANCEMENT: consider increasing speed and/or decreasing paddle width between levels
 //ENHANCEMENT: look for mode button during game end display, to allow skipping delay and mode selection screen
 //ENHANCEMENT: consider providing a bailout button - at least in auto mode
 //ENHANCEMENT: further tuning of tilt paddle response is needed
@@ -41,7 +35,7 @@ const char winTxt[] = "YOU WIN!!!";
 typedef struct {
 	unsigned long initialSpeedDelayMillis;		//initial 'speed' of ball (this is actually a millis delay between ball moves)
 	int paddleW;								//paddle width in pixels
-	int paddleSections;							//each half of the paddle is divided into this many sections, with the innermost section of each side being combined into one center section. The center section imparts zero influence on the X travel of a ball. Each succeeding outer section imparts one (positive or negative, depending on ball direction) unit to the ball's horizontal direction, up to the max value of an outermost section.
+	int paddleSections;							//each *half* of the paddle is divided into this many sections, with the innermost section of each side being combined into one center section that is twice the width of the other sections. The center section imparts zero influence on the X travel of a ball. Each succeeding outer section imparts one (positive or negative, depending on ball direction) unit to the ball's horizontal direction, up to the max value of an outermost section.
 	int speedIncreaseHitCount;					//number of ball/paddle hits between progressive speed increases
 	unsigned long speedIncreaseIncrementMillis; //number of millis by which to increase ball speed at each progressive increase (actually decrease in ball delay)
 	unsigned long maxSpeedDelayMillis;			//'max' delay millis (actually minimum...), which sets max speed for ball
@@ -59,7 +53,7 @@ const int ballW = 4;					 	//ball size in px (ball is assumed to be square)
 int ballX = 0;								//horizontal position on screen of top left corner of ball
 int ballY = 0;								//vertical position on screen of top left corner of ball
 int ballXComp = 1;							//X component of ball vector (negative = movement left, positive = movement right)
-int ballYComp = -2;							//Y component of ball vector (negative = movement down the screen, positive = movement up the screen)
+int ballYComp = 2;							//Y component of ball vector (negative = movement up the screen, positive = movement down the screen)
 unsigned long ballProcessDelay = 0;					 //milliseconds between processing of ball - controls ball speed (set from mode params)
 int paddleW = 0;							//paddle width in px - set from mode params, then shrinks at each level progression (if/as specified in mode params)
 int paddleX = 0;							//horizontal position on screen of top left corner of paddle
@@ -69,6 +63,7 @@ int paddleDivisionW = 0;					//width in px of each division of the paddle, as de
 
 const int numBricksW = 16;					//number of bricks on screen horizontally
 //TEMP const int maxBricksH = 16;					//max number of bricks on screen vertically - this is a physical limitation of how many bricks will fit on screen, and still allow paddle and ball movement below. (if exceeded (due to increased brick rows per screen, know that game is over)
+//should be const for simplified version, but splash screen is using this in 'upscale' version
 int maxBricksH = 24;
 int numBricksH = 10;						//number of bricks on screen vertically, at present (grows at each level progression)
 const int brickW = 10;						//width of bricks in pixels
@@ -78,9 +73,10 @@ int numBricks = 0;							//tracks how many bricks are drawn for a level
 int numBricksHit = 0;						//track how many bricks have been hit in current level (compare num to numHit, to determine when level is complete)
 int score = 0;								//track score for game
 //TEMP int bricks[numBricksW][maxBricksH];			//2D array of bricks - tracks if each individual brick is active, and its point value, if so (0 = inactive, >0 = point value
+//again, when maxBricksH is a constant, then it can be used in array declaration (such as for simplified version), but for upscale version, its not constant, so can't be used for array dimension
 int bricks[numBricksW][24];
-//**REFACTOR - if this won't work, just trash it
-int colBrickCount[numBricksW];
+//**REFACTOR - if this won't work for improving auto mode, just trash it (at least remove for simpler version?)
+int colBrickCount[numBricksW];				//track number of bricks remaining in each brick column
 
 const int startLives = 3;					//number of lives that a game starts with
 int lives = startLives;						//counter for number of lives left
@@ -110,18 +106,6 @@ const int levelX = levelLblX + strlen(levelLbl) * 6;
 const int livesX = livesLblX + strlen(livesLbl) * 6 + 2;
 
 int tiltZeroOffset = 0;							//level offset used to compensate for a non-level 'zero' position when tilt mode is selected
-
-//int freeRam () {
-//  extern int __heap_start, *__brkval;
-//  int v;
-//  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-//  }
-//
-//char *getFreeRamAsText() {
-//	static char sBuf[11] = {'\0'};
-//	sprintf_P(sBuf, PSTR("%d bytes"), freeRam());
-//	return sBuf;
-//}
 
 void setupModeParams() {
 	modeParams[JOYSTICK].initialSpeedDelayMillis = 30;
@@ -167,7 +151,7 @@ void setup() {
 
 	setupModeParams();
 
-	showSpashScreen();
+//	showSpashScreen();
 
 	newGame();
 }
@@ -252,7 +236,7 @@ void readPaddleTilt() {
 	static const int tiltDeadZone = 2;					//amount of wiggle room allowed, in which tilt movement is ignored (to prevent a hyper-sensitive tilt response)
 	static const unsigned long tiltDelayMillis = 2;		//milliseconds delay between processing tilt reading (again, to de-tune the tilt response a bit)
 	static const int maxRange = 4;						//max amount to add to paddle position on each read (smaller value leads to less drastic paddle movement)
-	static const float smoothAlpha = 0.7;				//smoothing factor, > 0 and < 1 - larger value dampens movement more, by weighting prior value more than current value
+	static const float smoothAlpha = 0.67;				//smoothing factor, > 0 and < 1 - larger value dampens movement more, by weighting prior value more than current value
 	static int smoothVal = 0;							//smoothed value, calculated from current value, last value (because its static) and smoothing factor
 	static unsigned long lastMillis = 0;				//last time the tilt value was read
 	int tiltVal = Esplora.readAccelerometer(X_AXIS) - tiltZeroOffset;	//the actual, 'raw' value of the tilt sensor (-512 to 512)
@@ -263,11 +247,10 @@ void readPaddleTilt() {
 		//if haven't moved out of the 'dead' zone (right in the middle), don't move the paddle - provides some stability while trying to hold still
 		if (abs(tiltVal) > tiltDeadZone) {
 
-			//keep the tilt value within the min/max range
-			if (tiltVal < -1*maxRange) {
-				tiltVal = -1*maxRange;
-			} else if (tiltVal > maxRange) {
-				tiltVal = maxRange;
+			//keep tiltVal within +- maxRange
+			if (abs(tiltVal) > maxRange) {
+				//(val > 0) - (val < 0) provides a 'sign' value, where val< 0=>-1, val>0=>1
+				tiltVal = (int)((tiltVal > 0) - (tiltVal < 0)) * maxRange;
 			}
 
 			//because smoothVal is static, this calculation uses the prior smooth value in calculating the new smooth value
@@ -276,6 +259,7 @@ void readPaddleTilt() {
 
 		}
 
+		//keep track of when this processed, so can wait the appropriate time before processing again
 		lastMillis = millis();
 	}
 }
@@ -285,18 +269,13 @@ void readPaddleAuto() {
 	int col1;
 	int col2;
 	mapBallToCol(&col1, &col2);
-//	if (colBrickCount[col1] == 0 || colBrickCount[col2] == 0) {
-		//Esplora.writeRGB(255, 0, 0);
-//		rgbWrite(255, 0, 0, 8);
-//	} else {
-//		Esplora.writeRGB(0, 255, 0);
-//		rgbWrite(0, 255, 0, 8);
-//	}
-	//keep the paddle under the ball, but randomly move the paddle around by a paddle sectyion, to prevent
+
+	//keep the paddle under the ball, but randomly move the paddle around by a paddle section, to prevent
 	// getting 'stuck' in one place, such as in the case of a straight vertical hit
 	paddleX = ballX - paddleW/2 + ballW/2;
 
-	if ((ballY > paddleY - ballYComp - ballW - 1 && ballY < paddleY) && ballYComp > 0) {
+	//if ball is moving downward, and ballY < paddleY (ie, above paddle), and ball is within a ball width of the paddle (ie, about to hit paddle)
+	if (ballYComp > 0 && (ballY < paddleY && ballY > paddleY - ballYComp - ballW - 1)) {
 		//if ball is alont
 		if (ballX < paddleW/2 - ballW/2) {
 
@@ -345,18 +324,28 @@ void readPaddleJoystick() {
 	}
 }
 
+//determine brick column from ball position (used by autoplay mode)
+//ball can possibly span 2 columns (positioned in space 'between' columns), so alters both
+//passed pointer values. If only in one column, both values are the same.
 void mapBallToCol(int* col1, int* col2) {
+	//make sure the ball is actually within the width of the bricks
 	if (ballX >= brickW && ballX <= brickW*numBricksW + brickW - 1) {
+		//determine first column
 		*col1 = ballX/brickW - 1;
+
+		//if ball spans into another column, set value for 2nd column
 		if (ballX >= *col1 * brickW + brickW + ballW + 1) {
 			*col2 = *col1 + 1;
 
+			//if 2nd col is outside of width of bricks, set 2nd value to first value (ie, doesn't actually span)
 			if (*col2 > numBricksW - 1) {
 				*col2 = *col1;
 			}
+		//if doesn't span columns, return same value for both pointer values
 		} else {
 			*col2 = *col1;
 		}
+	//if outside of width of bricks, return -1 for both pointer values
 	} else {
 		*col1 = -1;
 		*col2 = -1;
@@ -364,15 +353,30 @@ void mapBallToCol(int* col1, int* col2) {
 }
 
 //REFACTOR - examine the *paddle methods, and make sure they're being used/called sensibly
+//Setup a new paddle (called at start of game, after lost ball and between levels)
 void newPaddle() {
 	int effectivePaddleW = 0;
 
-	EsploraTFT.fill(0, 0, 0);				//erase the entire paddle area
+	//erase the entire paddle area
+	EsploraTFT.fill(0, 0, 0);
 	EsploraTFT.rect(0, paddleY, screenW, paddleH);
-	ballProcessDelay = modeParams[paddleMode].initialSpeedDelayMillis;
 	EsploraTFT.noStroke();
 
-	effectivePaddleW = paddleW/2 - ballW/2 + ballW - 1;
+	//REFACTOR - this should probably be done elsewhere, vs in 'newPaddle'?
+	//setup ball
+	ballProcessDelay = modeParams[paddleMode].initialSpeedDelayMillis;
+
+	//determine how to divide the paddle up into the specified number of sections (from modeParams)
+	// to achieve proper horizontal component of bounce off of paddle
+	//start by determining the 'effective' paddle width (ie, what range of ball positions will result
+	// in a hit of the ball - left edge of ball could be off left edge of paddle, for instance, but
+	// still result in a hit of the ball, due to overlap of the right edge of the ball with the left
+	// edge of the paddle, thus the paddle is effectively wider than the number of pixels it occupies
+	//**NOTE that this 'effectiveWidth" is actually the effective width of one half of the paddle
+//	effectivePaddleW = paddleW/2 - ballW/2 + ballW - 1;
+	effectivePaddleW = paddleW/2 + ballW - 1;
+//	effectivePaddleW = (paddleW + ballW - 1)/2;
+//	effectivePaddleW = paddleW;//(paddleW)/2;
 	paddleDivisionW = effectivePaddleW/modeParams[paddleMode].paddleSections + 1;
 
 	lastPaddleX = -1;
@@ -507,10 +511,6 @@ void processBall(void) {
 						if (colBrickCount[a] > 0) {
 							colBrickCount[a]--;				//reduce count of bricks in this column
 						}
-	//					Serial.print("col:ct ");
-	//					Serial.print(a);
-	//					Serial.print(":");
-	//					Serial.println(colBrickCount[a]);
 					}
 				}
 			}
@@ -519,6 +519,7 @@ void processBall(void) {
 		if (oldHits != numBricksHit) {
 			showScore();
 		}
+
 		//check if the ball hits the paddle
 		if (ballX > paddleX - ballXComp - ballW
 				&& ballX < paddleX + paddleW + ballXComp*-1
@@ -694,7 +695,6 @@ void showCountdown() {
 
 		EsploraTFT.stroke(0, 0, 255);
 		EsploraTFT.textSize(2);
-		//EsploraTFT.text(secsBuff, countdownTxtX, countdownTxtY);
 		EsploraTFT.text(secsBuff, countdownTxtX, numBricksH * brickH + screenTopY + belowBricksTextMarginH);
 		EsploraTFT.noStroke();
 		delayWithPaddle(1000);
@@ -822,10 +822,8 @@ void newScreen(void) {
 
 void newBall() {
 	ballX = random(screenW - ballW);
-//	ballX = screenX/2 - ballW/2;
 	ballY = screenTopY + numBricksH * brickH + screenTopY;
 	ballXComp = map(random(3), 0, 2, -1, 1);
-//	ballXComp = 0;
 	ballYComp = abs(ballYComp);
 }
 
@@ -844,30 +842,12 @@ void setupBricks(void) {
 			} else {
 				bricks[a][b] = 0;
 			}
-//			Serial.print(brick[a][b]);
-//			Serial.print(":");
-//			if (b < 3 && a > 10) {
-//				brick[a][b] = HIGH;
-//				colBrickCount[a]++;
-//			} else {
-//				brick[a][b] = LOW;
-//			}
 		}
 		colBrickCount[a] = numBricksH;
-//		Serial.println();
 	}
 
 	//Esplora uses order BGR, vs RGB - go figure...
 	const unsigned char brickColors[12][3] = {
-//			{0, 0, 255},		//red
-//			{0, 100, 255},		//orange
-//			{225, 0, 255},		//pink
-//			{255, 51, 153},		//purple
-//			{255, 51, 51},		//blue
-//			{255, 204, 229},	//lavender
-//			{0, 255, 0},		//green
-//			{0, 255, 255}		//yellow
-
 			{0, 0, 255},		//red
 			{0, 100, 255},		//orange
 			{225, 0, 255},		//pink
@@ -897,32 +877,3 @@ void setupBricks(void) {
 		}
 	}
 }
-
-////**TODO: Move to EsporaJimS library
-//const byte RED_PIN    = 5;
-//const byte BLUE_PIN   = 9;
-//const byte GREEN_PIN  = 10;
-//
-//void rgbWrite(byte r, byte g, byte b, byte i) {
-//	static byte lastR = 0;
-//	static byte lastG = 0;
-//	static byte lastB = 0;
-//	byte iR = map(r, 0, 255, 0, i);
-//	byte iG = map(g, 0, 255, 0, i);
-//	byte iB = map(b, 0, 255, 0, i);
-//
-//	if (r != lastR) {
-//		analogWrite(RED_PIN, iR);
-//		lastR = iR;
-//	}
-//	if (g != lastG) {
-//		analogWrite(GREEN_PIN, iG);
-//		lastG = iG;
-//	}
-//	if (b != lastB) {
-//		analogWrite(BLUE_PIN, iB);
-//		lastB = iB;
-//	}
-//}
-
-
